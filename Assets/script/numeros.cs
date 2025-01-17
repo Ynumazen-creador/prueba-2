25a9c99b251a34b426438c6e152e6ab84eeeee2b
el mero <rata@gmail.com>
2025-01-16 20:10:18 -0500

    fff
    fffff

---
+++ Assets/script/numeros.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System; 
+
+public class numeros : MonoBehaviour
+{
+    public Text numeroTope; //numero maximo
+    public Text numeroTexto;
+    int numeroInicio;
+    int numeroActual;
+    int numeroCabeza; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        numeroCabeza = 0; 
+           numeroInicio = 0;
+        numeroInicio = numeroActual; 
+        numeroTexto.text = numeroInicio.ToString();  
+    }
+
+    public void next() {
+      
+        numeroActual++;
+        numeroTexto.text = numeroActual.ToString();
+        aaa(); 
+    }
+
+    public void prev() {
+        numeroActual--;
+        numeroTexto.text = numeroActual.ToString();
+
+        numeroCabeza = int.Parse(numeroTope.text);
+        if (numeroActual == 0)
+        {
+            numeroActual = numeroCabeza+1;
+        }
+    }
+
+    void aaa() {
+
+        numeroCabeza = int.Parse(numeroTope.text);
+        if (numeroActual == numeroCabeza)
+        {
+            numeroActual = -1;
+        }
+    }
+
+    public void reinicio(){
+        numeroActual=0 ;
+        numeroTexto.text = 0.ToString(); 
+
+    }
+}
